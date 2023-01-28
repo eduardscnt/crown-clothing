f79014867e9d6cd3249c15606ce6dde9f21e06ac
SparkFlowGt <scinteiestefan@gmail.com>
2023-01-28 20:34:10 +0200

    cart

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState,useEffect } from "react";
 
-import { onAuthStateChangedListener,  } from "../utils/firebase/firebase.utils";
+import { createUserDocumentFromAuth, onAuthStateChangedListener,  } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
   setCurrentUser: () => null,
@@ -15,7 +15,12 @@ export const UserProvider = ({ children }) => {
   
 
   useEffect(()=>{
-     const unsubscribe = onAuthStateChangedListener((user)=>{console.log(user);})
+     const unsubscribe = onAuthStateChangedListener((user)=>{
+      if(user){
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
 
      return unsubscribe;
   },[])
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { PructsProvider } from "./contexts/products.context";
+import { CartProvider } from "./contexts/cart.context";
 
 import "./index.scss";
 
@@ -11,10 +13,14 @@ const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+  <BrowserRouter>
+       <UserProvider>
+         <PructsProvider>
+           <CartProvider>
+            <App />
+          </CartProvider>
+         </PructsProvider>
+       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   rootElement
