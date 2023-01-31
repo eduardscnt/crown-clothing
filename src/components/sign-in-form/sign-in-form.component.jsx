24ba868a74e3b1cb1d97fb70d84b753c48ac3339
SparkFlowGt <scinteiestefan@gmail.com>
2023-01-31 12:18:12 +0200

    added styled sign in form

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -6,10 +6,10 @@ import Button ,{BUTTON_TYPE_CLASSES} from '../button/button.component';
 import {
   signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup,
-  createUserDocumentFromAuth,
+  
 } from '../../utils/firebase/firebase.utils';
 
-import './sign-in-form.styles.scss';
+import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
 
 
 const defaultFormFields = {
@@ -47,7 +47,7 @@ const SignInForm = () => {
   };
 
   return (
-    <div className='sign-in-container'>
+    <SignInContainer>
       <h2>Already have an account?</h2>
       <span>Sign in with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -68,14 +68,14 @@ const SignInForm = () => {
           name='password'
           value={password}
         />
-        <div className='buttons-container'>
+        <ButtonsContainer>
           <Button type='submit'>Sign In</Button>
           <Button buttonType={BUTTON_TYPE_CLASSES.google} type='button' onClick={signInWithGoogle}>
             Google Sign In
           </Button>
-        </div>
+        </ButtonsContainer>
       </form>
-    </div>
+    </SignInContainer>
   );
 };
 
