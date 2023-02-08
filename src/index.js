ca5155a5087995e0ef456e1fecb1ee8dd60fa8f2
SparkFlowGt <scinteiestefan@gmail.com>
2023-02-08 13:13:51 +0200

    converted user-context to user-redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
-
+import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
+import { store } from "./store/store";
 
 import "./index.scss";
 
@@ -13,15 +14,15 @@ const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
+  <Provider store={store}>
   <BrowserRouter>
-       <UserProvider>
-        
+          <CategoriesProvider>
            <CartProvider>
-            <App />
-          </CartProvider>
-       
-       </UserProvider>
+              <App />
+           </CartProvider>
+          </CategoriesProvider>
     </BrowserRouter>
+  </Provider>
   </React.StrictMode>,
   rootElement
 );
